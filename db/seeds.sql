4fb627ec1f55187d8452030ef682c9a79f78dd5d
crosgar <cecilia.rossi.garcia@gmail.com>
2021-06-01 15:06:48 -0700

    set up project folders and files, started sql logic, set up first table

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES 
+   ('Cecilia', 'Rossi', 1),
+   ('Octavia', 'Butler', 1),
+   ('Luke', 'Skywalker', 0),
+   ('Andie', 'Lucia', 0),
+   ('Carlos', 'Manuel', 1),
+   ('Hebe', 'Garcia', 0),
+   ('Laura', 'Benitez', 1),
+   ('Sara', 'Cardona', 1),
+   ('Olga', 'Guillot', 0),
+   ('Celia', 'Cruz', 1);
