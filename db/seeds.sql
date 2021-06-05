6d419ee10a3fe61f0e8298ef0f9d5394f9ef9a82
crosgar <cecilia.rossi.garcia@gmail.com>
2021-06-04 20:24:51 -0700

    added foreign key to candidates table to ref parties table, added JOIN statements to api calls

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,15 +5,15 @@ VALUES
   ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
 
 
-INSERT INTO candidates (first_name, last_name, industry_connected)
+INSERT INTO candidates (first_name, last_name, party_id, industry_connected)
 VALUES 
-   ('Cecilia', 'Rossi', 1),
-   ('Octavia', 'Butler', 1),
-   ('Luke', 'Skywalker', 0),
-   ('Andie', 'Lucia', 0),
-   ('Carlos', 'Manuel', 1),
-   ('Hebe', 'Garcia', 0),
-   ('Laura', 'Benitez', 1),
-   ('Sara', 'Cardona', 1),
-   ('Olga', 'Guillot', 0),
-   ('Celia', 'Cruz', 1);
+   ('Cecilia', 'Rossi', 3, 1),
+   ('Octavia', 'Butler', 3, 1),
+   ('Luke', 'Skywalker', 1, 0),
+   ('Andie', 'Lucia', 2, 0),
+   ('Carlos', 'Manuel', 1, 1),
+   ('Hebe', 'Garcia', 2, 0),
+   ('Laura', 'Benitez', 2, 1),
+   ('Sara', 'Cardona', 1, 1),
+   ('Olga', 'Guillot', 3, 0),
+   ('Celia', 'Cruz', NULL, 1);
